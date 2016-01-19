b4ce16e9a5a46d692e5bc1cfbe1648ff9de61489
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2016-01-19 01:02:04 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs;
+npm install;
+
+sudo apt-get install xdotool google-chrome-stable espeak;
