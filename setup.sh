e8bafcda45b3a57afb0ee0a54374536fecd32b9d
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2016-01-31 18:44:36 +0000

    u

--- setup.sh
+++ setup.sh
@@ -6,31 +6,33 @@ apt-get update
 apt-get install bison libasound2-dev swig python-dev mplayer
 
 # build sphinxbase
-cd ~/
-wget http://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz
-tar -zxvf ./sphinxbase-5prealpha.tar.gz
-cd ./sphinxbase-5prealpha
-./configure --enable-fixed
-make clean all
-make check
-sudo make install
+# cd ~/
+# wget http://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz
+# tar -zxvf ./sphinxbase-5prealpha.tar.gz
+# cd ./sphinxbase-5prealpha
+# ./configure --enable-fixed
+# make clean all
+# make check
+# sudo make install
 
-# build pocketsphinx
-cd ~/
-wget http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz
-tar -zxvf ./pocketsphinx-5prealpha.tar.gz
-cd ./pocketsphinx-5prealpha
-./configure
-make clean all
-make check
-sudo make install
+# # build pocketsphinx
+# cd ~/
+# wget http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz
+# tar -zxvf ./pocketsphinx-5prealpha.tar.gz
+# cd ./pocketsphinx-5prealpha
+# ./configure
+# make clean all
+# make check
+# sudo make install
 
 # miscellaneous dependencies
-apt-get install xautomation espeak
+apt-get install xautomation espeak chromium-browser
 
 # install node
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get install -y nodejs
+curl -sLS https://apt.adafruit.com/add | sudo bash
+apt-get install node
 
 # install mrrobot
 cd ~/mrrobot
